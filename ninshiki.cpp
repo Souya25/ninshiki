4e9ba9fef7ab6625f7673e4d2acbb4fab96d4b94
Souya <amatou08252525@gmail.com>
2020-07-12 01:44:50 +0900

    fin

--- ninshiki.cpp
+++ ninshiki.cpp
@@ -3,10 +3,11 @@
 #include<sstream>
 #include<iomanip>
 #include<fstream>
+#include<vector>
+#include<math.h>
 
 using namespace std;
-
-
+const int frame_max = 15;
 
 string makepath(string file_path, int file_num)
 {   
@@ -17,38 +18,159 @@ string makepath(string file_path, int file_num)
     return path_str = path.str();
 }
 
-
-
-int main()
+vector<string> split(string str, string separator)//文字列を分割しvectorに格納する関数
 {
-    std::ifstream ifs;
-    int  i, j, k, l, score = 0;
-    string file_path[] = { "city011", "city012", "city021", "city022"};
-    string f1_path, f2_path, f1_data, f2_data;
-    for(i = 0; i < 4; i++)
-    {
-        if(i != j)
-        {
-            for(j =0; j < 4; j++)
-            {
-                for( k = 1; k < 101; k++)
-                {
-                    f1_path = makepath(file_path[i], k);
-                    for( l = 1; l < 101; l++)
-                    {
-                        //cout << f1_path << endl;
-                        f2_path = makepath(file_path[j], l);
-                        f2_path = "cpninshiki,.py";
-                        ifs.open(f2_path);
-                        while(!ifs.eof())
-                            getline(ifs, f2_data);
-                            cout << f2_data;
+    vector<string> result;
+    string tstr = str + separator;
+    long l = tstr.length(), sl = separator.length();
+    string::size_type pos = 0, prev = 0;
 
-                    }
-                }
+    for (;pos < l && (pos = tstr.find(separator, pos)) != string::npos; prev = (pos += sl)) 
+    {
+        result.emplace_back(tstr, prev, pos - prev);
+    }
+    
+    return result;
+}
+
+
+double local_distance(string f1_path, string f2_path)
+{   
+    vector< vector< string>> f1_vec(160), f2_vec(160);
+    vector<string> f_line;
+    ifstream f1(f1_path), f2(f2_path);
+    string f1_data, f2_data;
+    int f1_size = 0, f2_size = 0, i = 0, j, k;
+    double ans, cum_up, cum_diag, cum_left;
+    while( getline( f1, f1_data))
+    {   
+        if(i > 2)
+        {
+            f_line = split(f1_data, " ");
+            
+            for( j = 0; j < 15; j++)
+            {
+                f1_vec[i-3].emplace_back(f_line[j]);
+            }
+        }
+    i++;
+    }
+    f1_size = i-3;
+    i = 0;
+    while( getline( f2, f2_data))
+    {   
+        if(i > 2)
+        {
+            f_line = split(f2_data, " ");
+            
+            for( j = 0; j < 15; j++)
+            {
+                f2_vec[i-3].emplace_back(f_line[j]);       
+            }
+        }
+    i++;
+    }
+    f2_size = i-3;
+    
+    f1.close();
+    f2.close();
+    vector< vector< double>> ld_vec(f1_size, vector<double>(f2_size)), cum_dis_vec(f1_size, vector<double>(f2_size));
+    //局所距離を求める
+    for(i = 0; i < f1_size; i++)
+    {
+        for(j = 0; j < f2_size; j++)
+        {
+            for(k = 0; k < 15; k++)
+            {
+                ans += pow( stod(f1_vec[i][k]) - stod(f2_vec[j][k]),2);
+            }
+            ld_vec[i][j] = sqrt(ans);
+            ans = 0;
+        }
+    }
+    //初期条件
+    cum_dis_vec[0][0] = ld_vec[0][0];
+    //境界条件
+    for( i = 1; i < f1_size; i++)
+    {
+        cum_dis_vec[i][0] = cum_dis_vec[i-1][0] + ld_vec[i][0];
+    }
+    for( j = 1; j < f2_size; j++)
+    {
+        cum_dis_vec[0][j] = cum_dis_vec[0][j-1] + ld_vec[0][j];
+    }
+    //累積距離
+    for(i = 1; i < f1_size; i++)
+    {
+        for(j = 1; j < f2_size; j++)
+        {
+            cum_up = cum_dis_vec[i-1][j] +  ld_vec[i][j];
+            cum_diag = cum_dis_vec[i-1][j-1] + 2 * ld_vec[i][j];//斜め遷移の計算
+            cum_left = cum_dis_vec[i][j-1] +  ld_vec[i][j];
+            if(cum_up <= cum_diag && cum_up <= cum_left)
+            {
+                cum_dis_vec[i][j] = cum_up;
+            }
+            else if(cum_diag <= cum_up && cum_diag <= cum_left)
+            {
+                cum_dis_vec[i][j] = cum_diag;
+            }
+            else if(cum_left <= cum_diag && cum_left <= cum_up)
+            {
+                cum_dis_vec[i][j] = cum_left;
             }
         }
     }
-    
+    return ( cum_dis_vec[ f1_size - 1][ f2_size - 1]/ ( f1_size + f2_size));
+}
 
+int main()
+{
+    vector<string> f_line;
+    vector< vector< double>> ld;
+    double total, total_min=-1, percent;
+    std::ifstream ifs;
+    int  i, j, k, l, score = 0 ,min_num, count=0;
+    string file_path[] = { "city011", "city012", "city021", "city022"};
+    string f1_path, f2_path, f1_data, f2_data, data;
+    for(i = 0; i < 4; i++)
+    {    
+        for(j =0; j < 4; j++)
+        {
+            if(i != j)
+            {
+                cout << "=====" << file_path[i] <<"と"<<file_path[j]<<"のマッチング結果====="<<endl;
+                for( k = 1; k < 101; k++)
+                {
+                    for( l = 1; l < 101; l++)
+                    {
+                        //pathを作成
+                        f1_path = makepath(file_path[i], k);
+                        f2_path = makepath(file_path[j], l);
+                        //マッチング
+                        total = local_distance( f1_path, f2_path);
+                        //最終の累積距離を比較
+                        if(total_min == -1 || total_min >= total)
+                        {
+                            total_min = total;
+                            min_num = l;
+                        }
+                    }
+
+                    //正答率を計算
+                    if(min_num == k)
+                    {
+                        count++;
+                    }else
+                    {
+                        cout << "ERROR:  " << f1_path  <<",   "<< makepath(file_path[j], min_num) << endl;
+                    }
+                    total_min = -1;
+                }
+                //結果を出力
+                cout<<"正答率:"<< count << "%"<< endl;
+                count = 0;
+            }
+        }
+    }
 }
---
+++ ninshiki.py
@@ -0,0 +1,116 @@
+import math
+import copy
+import numpy as np
+import time
+
+
+def make_path(file_path, file_num):
+    num_str = str(file_num)
+    num_zero = num_str.zfill(3)
+    path = f1_path = "{0}/{1}_{2}.txt".format( file_path, file_path, num_zero)
+    return path
+
+def local_distance(f1 ,f2):
+    i = 0
+    j = 0
+    ans = 0
+    dim = f1.readline().split()
+    dim = f2.readline().split()
+    dim = f1.readline().split()
+    dim = f2.readline().split()
+    dim = f1.readline().split()
+    dim = f2.readline().split()
+    dim1 = np.empty( (0,15) , str)
+    dim2 = np.empty( (0,15) , str)
+    #print(dim1,dim2)
+    for line1 in f1:
+        dim1 = np.append(dim1, np.array([line1.split()]), axis = 0 )
+        i += 1
+    for line2 in f2:
+        dim2 = np.append(dim2, np.array([line2.split()]), axis = 0)
+        j += 1
+    local_d = np.empty((0,j),float)
+    for i2 in range(i):
+        #print(local_d)     
+        list_ans =np.empty(0,float)
+        for j2 in range(j):
+            for k in range (0,15):
+                ans += pow( float(dim1[i2][k]) - float(dim2[j2][k]), 2)
+            list_ans = np.append(list_ans, math.sqrt(ans))
+            ans = 0
+            #print(i2,j2)
+        #print(list_ans.shape)
+        local_d = np.append(local_d, [copy.deepcopy(list_ans)], axis = 0) 
+    #print(local_d.shape)
+    #return local_d
+
+    #def cumulative_distance(ld):
+    #print(ld)
+    cum_dis = np.empty((0,len(local_d[0])), float)
+    cum_up =0
+    i = 0
+    j = 0
+    for l in local_d:
+        cum_dis_line = np.empty(0,float)
+        j = 0
+        for l_i in l:
+            if i == 0 and j == 0:
+                #print(i)
+                cum_dis_line = np.append(cum_dis_line, l_i)
+            if i != 0:
+                cum_up = float(cum_dis[i-1][j]) + float(l_i)
+            if j != 0:
+                cum_left = float(cum_dis_line[j-1]) + float(l_i)
+            if i != 0 and j !=0:
+                cum_diag = float(cum_dis[i-1][j-1]) + (2 * float(l_i))
+                #print("up  {}, left  {}, diag  {}".format(cum_up, cum_left, cum_diag))
+                if cum_up <= cum_left and cum_up <= cum_diag:
+                    cum_dis_line = np.append(cum_dis_line, cum_up)
+                elif cum_diag <= cum_up and cum_diag <= cum_left:
+                    cum_dis_line = np.append(cum_dis_line, cum_diag)
+                else :
+                    cum_dis_line = np.append(cum_dis_line, cum_left)
+            elif i != 0:
+                #print("up  {}".format(cum_up))
+                cum_dis_line = np.append(cum_dis_line, cum_up)
+            elif j != 0:
+                #print("left  {}".format(cum_left))
+                cum_dis_line = np.append(cum_dis_line, cum_left)
+            #print(cum_dis_line[-1])
+            j+=1
+        cum_dis = np.append(cum_dis, [copy.deepcopy(cum_dis_line)], axis = 0)
+        i += 1
+    #print(cum_dis.shape)
+    #print(cum_dis[59])
+    t = cum_dis[-1][-1]/(i+j) #単語間距離 =　最終点までの累積距離 / i+j
+    return(t)
+
+
+
+file_path = np.array(["city011", "city012", "city021", "city022"])
+res = 0
+for i in range(0,4):
+    for j in range(0, 4):
+        score = 0
+        for k in range(1,101):
+            min_t_dis = 0
+            if j != i:
+                for l in range(1,101):
+                    f1_path = make_path(file_path[i], k) #テンプレート
+                    f1 = open(f1_path)
+                    f2_path = make_path(file_path[j], l)
+                    f2 = open(f2_path)
+                    t_dis = local_distance(f1,f2) #局所距離を得た
+                    f1.close()
+                    f2.close()
+                    
+                    if min_t_dis == 0 or min_t_dis >= t_dis:
+                        min_t_dis = t_dis
+                        min_num = l
+                    #print(min_num, min_t_dis ,t_dis, k,l, len(ld[0]))
+                if min_num == k:
+                    score += 1
+                    print("scored")
+                #print("fin101")
+
+                    
